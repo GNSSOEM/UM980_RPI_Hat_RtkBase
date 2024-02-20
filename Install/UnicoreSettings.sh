d42d1e0b4e8f6358d6c650e83eb948c16e79633c
JefWinKamaz <jef239@mail.ru>
2024-02-20 09:06:44 +0200

    Correct UnicoreSettings.sh

--- Install/UnicoreSettings.sh
+++ Install/UnicoreSettings.sh
@@ -4,6 +4,15 @@ settings="${rtkbase_path}"/settings.conf
 rtcm_msg="1005(10),1033(10),1077,1087,1097,1107,1117,1127"
 rtcm_msg_full="1005,1033,1019,1020,1042,1044,1045,1046,1077,1087,1097,1107,1117,1127"
 
+recvname=${1}
+if [[ ${recvname} == "" ]]
+then
+   recvfullname=Unicore
+else
+   recvfullname=Unicore_${recvname}
+fi
+#echo recvfullname=${recvfullname}
+
 #store service status before upgrade
 rtkbase_web_active==$(sudo systemctl is-active rtkbase_web.service)
 str2str_active=$(sudo systemctl is-active str2str_tcp)
@@ -18,16 +27,16 @@ str2str_file=$(sudo systemctl is-active str2str_file)
 [ $rtkbase_web_active = 'active' ] && sudo systemctl stop rtkbase_web.service
 [ $str2str_active = 'active' ] && sudo systemctl stop str2str_tcp
 [ $str2str_ntrip_A_active = 'active' ] && sudo systemctl stop str2str_ntrip_A
-[ $str2str_ntrip_B_active = 'active' ] && sudo systemctl stop str2str_ntrip_B  
+[ $str2str_ntrip_B_active = 'active' ] && sudo systemctl stop str2str_ntrip_B
 [ $str2str_local_caster = 'active' ] && sudo systemctl stop str2str_local_ntrip_caster
 [ $str2str_rtcm = 'active' ] && sudo systemctl stop str2str_rtcm_svr
 [ $str2str_serial = 'active' ] && sudo systemctl stop str2str_rtcm_serial
 [ $str2str_file = 'active' ] && sudo systemctl stop str2str_file
 
-sed -i s/^position=.*/position=\'0.00 0.00 0.00\'/ "${settings}"
+sed -i s/^position=.*/position=\'0\.00\ 0\.00\ 0\.00\'/ "${settings}"
 sed -i s/^com_port=.*/com_port=\'ttyS0\'/ "${settings}"
 sed -i s/^com_port_settings=.*/com_port_settings=\'115200:8:n:1\'/ "${settings}"
-sed -i s/^receiver=.*/receiver=\'Unicore_UM980\'/ "${settings}"
+sed -i s/^receiver=.*/receiver=\'${recvfullname}\'/ "${settings}"
 sed -i s/^receiver_format=.*/receiver_format=\'rtcm3\'/ "${settings}"
 sed -i s/^antenna_info=.*/antenna_info=\'ELT0123\'/ "${settings}"
 
@@ -47,7 +56,7 @@ sed -i s/^rtcm_serial_msg=.*/rtcm_serial_msg=\'${rtcm_msg_full}\'/ "${settings}"
 [ $rtkbase_web_active = 'active' ] && sudo systemctl start rtkbase_web.service
 [ $str2str_active = 'active' ] && sudo systemctl start str2str_tcp
 [ $str2str_ntrip_A_active = 'active' ] && sudo systemctl start str2str_ntrip_A
-[ $str2str_ntrip_B_active = 'active' ] && sudo systemctl start str2str_ntrip_B  
+[ $str2str_ntrip_B_active = 'active' ] && sudo systemctl start str2str_ntrip_B
 [ $str2str_local_caster = 'active' ] && sudo systemctl start str2str_local_ntrip_caster
 [ $str2str_rtcm = 'active' ] && sudo systemctl start str2str_rtcm_svr
 [ $str2str_serial = 'active' ] && sudo systemctl start str2str_rtcm_serial
