8026298cef562ccd5019cd06c53c6b6acff7af50
JefWinKamaz <jef239@mail.ru>
2024-07-20 20:50:15 +0300

    Don't rurn off USB power, if have USB-device

--- Install/tune_power.sh
+++ Install/tune_power.sh
@@ -7,19 +7,12 @@ PI4=`echo ${MODEL} | grep "Pi 4"`
 #echo PI4=${PI4}
 if [[ "${PI4}" != "" ]]
 then
-   if grep -q '^rtkbase_path=' /etc/environment
-   then
-      source /etc/environment
-   else
-      BASEDIR=`realpath $(dirname "$0")`
-      rtkbase_path=${BASEDIR}/rtkbase
-   fi
-   source <( grep '^com_port=' ${rtkbase_path}/settings.conf )
-   if [[ "${com_port}" == ttyUSB[0-9] ]] || [[ "${com_port}" == ttyACM[0-9] ]]
+   USB_COUNT=`lsusb -t | wc -l`
+   if [[ "${USB_COUNT}" -gt 3 ]]
    then
        USB_PORT=Y
    fi
-   #echo rtkbase_path=${rtkbase_path} com_port=${com_port} USB_PORT=${USB_PORT}
+   #echo USB_COUNT=${USB_COUNT} USB_PORT=${USB_PORT}
    if [[ "${USB_PORT}" != "Y" ]]
    then
       sudo uhubctl -l 1-1 -a 0 # turn off USB
