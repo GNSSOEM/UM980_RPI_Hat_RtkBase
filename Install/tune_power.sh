e3413fc802417343fe4599c468e4760952b7a755
JefWinKamaz <jef239@mail.ru>
2024-09-04 08:40:02 +0300

    Don't power off USB

--- Install/tune_power.sh
+++ Install/tune_power.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 sudo cpufreq-set -g powersave
-MODEL=`cat /proc/cpuinfo | grep Model`
+#MODEL=`cat /proc/cpuinfo | grep Model`
 #echo MODEL=${MODEL}
-PI4=`echo ${MODEL} | grep "Pi 4"`
+#PI4=`echo ${MODEL} | grep "Pi 4"`
 #echo PI4=${PI4}
-if [[ "${PI4}" != "" ]]
-then
-   USB_COUNT=`lsusb -t | wc -l`
-   if [[ "${USB_COUNT}" -gt 3 ]]
-   then
-       USB_PORT=Y
-   fi
-   #echo USB_COUNT=${USB_COUNT} USB_PORT=${USB_PORT}
-   if [[ "${USB_PORT}" != "Y" ]]
-   then
-      sudo uhubctl -l 1-1 -a 0 # turn off USB
-      #sudo uhubctl -l 2 -a 0
-   fi
-fi
+#if [[ "${PI4}" != "" ]]
+#then
+#   USB_COUNT=`lsusb -t | wc -l`
+#   if [[ "${USB_COUNT}" -gt 3 ]]
+#   then
+#       USB_PORT=Y
+#   fi
+#   #echo USB_COUNT=${USB_COUNT} USB_PORT=${USB_PORT}
+#   if [[ "${USB_PORT}" != "Y" ]]
+#   then
+#      sudo uhubctl -l 1-1 -a 0 # turn off USB
+#      #sudo uhubctl -l 2 -a 0
+#   fi
+#fi
 sudo vcgencmd display_power 0
